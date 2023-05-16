b71c228dbd55080d9bc77288f62aad2053d37fb2
Halil <halil.seran.1135@gmail.com>
2023-05-16 21:43:48 +0300

    shoulderPress

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
+import { getData, storeData, containsKey } from "../storage";
+import data from "../data.json";
 
 export default function useCachedResources() {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -7,12 +9,19 @@ export default function useCachedResources() {
   useEffect(() => {
     async function loadResourcesAndDataAsyc() {
       try {
+        const hasWorkouts = await containsKey("workout-data");
+        if (!hasWorkouts) {
+          console.log("storing data");
+          await storeData("workout-data", data);
+        }
         await Font.loadAsync({
           meera: require("../assets/fonts/MeeraInimai-Regular.ttf"),
         });
       } catch (e) {
         console.warn(e);
       } finally {
+        const workouts = await getData("workout-data");
+        console.log(workouts);
         setIsLoadingComplete(true);
       }
     }
